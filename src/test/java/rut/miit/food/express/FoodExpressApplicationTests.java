643b1096a5736d11e0cd2200459567863ec1255f
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2024-10-23 00:06:44 +0300

    Initial commit

---
+++ src/test/java/rut/miit/food/express/FoodExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package rut.miit.food.express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodExpressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
