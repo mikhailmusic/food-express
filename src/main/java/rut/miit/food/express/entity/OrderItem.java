b7185fc547126143c2c95d5f6962185a9b81affc
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2024-11-03 16:19:31 +0300

    entity: remove order_price

--- src/main/java/rut/miit/food/express/entity/OrderItem.java
+++ src/main/java/rut/miit/food/express/entity/OrderItem.java
@@ -7,23 +7,16 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "order_items")
 public class OrderItem extends BaseEntity {
-    private BigDecimal orderPrice;
     private Integer count;
     private Order order;
     private Dish dish;
 
-    public OrderItem(BigDecimal orderPrice, Integer count, Dish dish) {
-        this.orderPrice = orderPrice;
+    public OrderItem(Integer count, Dish dish) {
         this.count = count;
         this.dish = dish;
     }
 
     protected OrderItem() {
-    }
-
-    @Column(name = "order_price", nullable = false)
-    public BigDecimal getOrderPrice() {
-        return orderPrice;
     }
 
     @Column(name = "count", nullable = false)
@@ -41,10 +34,6 @@ public class OrderItem extends BaseEntity {
     @JoinColumn(name = "dish_id", nullable = false)
     public Dish getDish() {
         return dish;
-    }
-
-    public void setOrderPrice(BigDecimal orderPrice) {
-        this.orderPrice = orderPrice;
     }
 
     public void setCount(Integer count) {
